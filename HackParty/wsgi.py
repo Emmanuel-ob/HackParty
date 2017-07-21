36e1353fe156e69a08592122f8545b17d32d695d
Emmanuel Obute <emmanuel6.obute@gmail.com>
2017-07-21 13:50:05 +0100

    Prep for deployment

--- HackParty/wsgi.py
+++ HackParty/wsgi.py
@@ -8,15 +8,18 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+
 from dj_static import Cling
 from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HackParty.settings")
 
 #application = DjangoWhiteNoise(HackParty1)
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
 
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HackParty.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HackParty.settings")
 
 # application = Cling(get_wsgi_application())
