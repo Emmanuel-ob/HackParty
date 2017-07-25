acdd614f1aacf707485224a981c588303de4c84a
Emmanuel Obute <emmanuel6.obute@gmail.com>
2017-07-25 11:40:19 +0100

    new update

--- postApp/admin.py
+++ postApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from newsfeed.models import Like, Comment, Post, DisLike, Reply
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ['posted_by', 'date_posted', 'is_hidden']
+	list_filter  = ['is_hidden']
+
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Post)
+admin.site.register(DisLike)
+admin.site.register(Reply)
+
