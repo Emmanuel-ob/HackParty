bc3c58b924ce1cce8e49730228fe39e6f4e98926
Emmanuel Obute <emmanuel6.obute@gmail.com>
2017-07-25 18:24:28 +0100

    update

--- HackParty/mail_info.py
+++ HackParty/mail_info.py
@@ -9,6 +9,6 @@
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'emmybunchgz@gmail.com'
-EMAIL_HOST_PASSWORD = 'obotu1989'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
